923d7f9b68db9130ac8fc0a7f87d1c5fbbe40282
Csenge <csengehubay@gmail.com>
2026-06-16 09:46:15 +0200

    check obj meta

--- mecanumbot_sensorprocess_smart/mecanumbot_onboard_cam_detect_people.py
+++ mecanumbot_sensorprocess_smart/mecanumbot_onboard_cam_detect_people.py
@@ -151,6 +151,7 @@ class DeepStreamPersonDetectNode(Node):
                 self.get_logger().info("Extracting user meta for object.")
                 self.get_logger().info(f"Object class_id: {obj_meta.class_id}, confidence: {obj_meta.confidence}")
                 self.get_logger().info(f'l_user: {l_user}')
+                self.get_logger().info(f'obj_meta: {obj_meta}')
                 while l_user is not None:
                     try:
                         user_meta = pyds.NvDsUserMeta.cast(l_user.data)
