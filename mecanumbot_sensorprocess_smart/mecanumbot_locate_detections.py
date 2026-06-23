59716d1aaa1651e72e9fd6388aeff75083bb3d24
Csenge <csengehubay@gmail.com>
2026-06-23 12:01:15 +0200

    add bound angle imaging

--- mecanumbot_sensorprocess_smart/mecanumbot_locate_detections.py
+++ mecanumbot_sensorprocess_smart/mecanumbot_locate_detections.py
@@ -289,10 +289,12 @@ class PersonLocateNode(Node):
             self.get_logger().info(f"Publishing {len(fused_poses.poses)} fused detections.")
             self.people_pub.publish(fused_poses)
         if self.people_left_FOV.poses:
+            self.get_logger().info(f"Publishing {len(self.people_left_FOV.poses)} left FOV detections.")
             self.people_left_FOV.stamp = self.cam_stamp 
             self.people_left_FOV_pub.publish(self.people_left_FOV)
             self.people_left_FOV.poses.clear()
         if self.people_right_FOV.poses:
+            self.get_logger().info(f"Publishing {len(self.people_right_FOV.poses)} right FOV detections.")
             self.people_right_FOV.stamp = self.cam_stamp
             self.people_right_FOV_pub.publish(self.people_right_FOV)
             self.people_right_FOV.poses.clear()
