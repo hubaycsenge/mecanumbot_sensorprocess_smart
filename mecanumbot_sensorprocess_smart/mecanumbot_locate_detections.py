c193d18716d114803ccd591e1e674f662846149e
Csenge <csengehubay@gmail.com>
2026-06-23 12:27:01 +0200

    fix syntax

--- mecanumbot_sensorprocess_smart/mecanumbot_locate_detections.py
+++ mecanumbot_sensorprocess_smart/mecanumbot_locate_detections.py
@@ -6,7 +6,7 @@ from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import PoseArray, Pose, Point, PoseWithCovarianceStamped
 from nav_msgs.msg import OccupancyGrid
 from tf2_ros import TransformListener, Buffer
-from transforms3d.euler import euler2quat
+from transforms3d.euler import euler2quat,quat2euler
 import math
 import numpy as np
 import copy
@@ -96,15 +96,15 @@ class PersonLocateNode(Node):
         ]
 
         # 3. Convert current quaternion to Euler angles (returns roll, pitch, yaw)
-        roll, pitch, yaw = transforms3d.euler.quat2euler(q_current)
+        roll, pitch, yaw = quat2euler(q_current)
 
         # 4. Add the relative yaw angles
         yaw_min = yaw + X_min
         yaw_max = yaw + X_max
 
         # 5. Convert back to quaternions: returns [w, x, y, z]
-        q_min = transforms3d.euler.euler2quat(roll, pitch, yaw_min)
-        q_max = transforms3d.euler.euler2quat(roll, pitch, yaw_max)
+        q_min = euler2quat(roll, pitch, yaw_min)
+        q_max = euler2quat(roll, pitch, yaw_max)
 
         # 6. Assign the new values back to our copied ROS poses (W is q[0])
         min_pose.orientation.w = q_min[0]
